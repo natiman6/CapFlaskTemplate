8a096db681310b30a711315995a86483512df6be
Nathan Negose <s_nathan.negose@ousd.org>
2022-05-13 10:07:07 -0700

    final commit

--- app/utils/secrets.py
+++ app/utils/secrets.py
@@ -1,14 +1,11 @@
 # The purpose of this file is to hold sensitive information that you don't want to 
 # post publicly to GitHub.  This file is excluded from being sent to github by .gitignore
 
-# For the email recovery using a gmail account to work you need to turn on "less secure apps" 
-# here https://myaccount.google.com/lesssecureapps
-
 def getSecrets():
     secrets = {
         'MAIL_PASSWORD':'00413289',
-        'MAIL_USERNAME':'natiman6',
-        'MONGO_ADMIN': 'Nathan Negose',
-        'MONGO_PASSWORD':'00413289'
+        'MAIL_USERNAME':'s_nathan.negose@ousd.org',
+        'MONGO_HOST':'mongodb+srv://nathannegose:Natiman6.com@cluster0.l1d7y.mongodb.net/capstone?retryWrites=true&w=majority',
+        'MONGO_DB_NAME':'capstone'
         }
     return secrets
